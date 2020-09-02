6adb6ee8bbcee1fbe6fed7304ec0aefe1eeca8b1
pavel-sobchenko <sobchanka@tut.by>
2020-09-03 02:19:02 +0300

    Updates

---
+++ precache-manifest.71b8309e9e29d36d3e78740aaad6e7c2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82684b9bb7d30b6977197a2364b2ba03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a17369c8141bfee4e0e4",
+    "url": "/static/css/2.c1ad54db.chunk.css"
+  },
+  {
+    "revision": "d99f317f007322c7e179",
+    "url": "/static/css/main.da942fe9.chunk.css"
+  },
+  {
+    "revision": "a17369c8141bfee4e0e4",
+    "url": "/static/js/2.a692f8a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a692f8a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d99f317f007322c7e179",
+    "url": "/static/js/main.a04e72b4.chunk.js"
+  },
+  {
+    "revision": "dc9173b7e8edec60da3a",
+    "url": "/static/js/runtime-main.3ffde9c1.js"
+  },
+  {
+    "revision": "0aba4c18507013baa5b7d46de284cd56",
+    "url": "/static/media/bird.0aba4c18.gif"
+  },
+  {
+    "revision": "7ce82e808cd4f65d964bdec2b9e20b07",
+    "url": "/static/media/eagle.7ce82e80.jpg"
+  }
+]);
